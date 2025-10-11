f09e1a79855d961ed7c4776d44dfa9671c0d6937
uma maheswari <uma maheswari@LAPTOP-PH0PAFSB>
2025-10-11 13:50:58 +0530

    java

---
+++ TNS/src/com/tnsif/nonaccessmodifiers/FinalDemo.java
@@ -0,0 +1,29 @@
+package com.tnsif.nonaccessmodifiers;
+
+public class FinalDemo {
+
+	final int Max = 100;
+	
+	 void display() {
+		System.out.println("Show the value of max:  "+Max);
+	}
+
+	public static void main(String[] args) {
+		FinalDemo fd = new FinalDemo();
+	//	fd.Max = 300;
+		fd.display();
+		
+		
+	}
+
+}
+
+class Child extends FinalDemo{
+	
+	
+	void display() {
+		System.out.println("Show the value of max:  "+Max);
+	}
+}
+
+
